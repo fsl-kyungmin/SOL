bab3dda6797ba753eb58c9cc649641ebc17e0ee7
fsl-kyungmin <fsl-kyungmin@fairsquarelab.com>
2025-03-07 19:37:38 +0900

    commit

--- deploy/spl-token.ts
+++ deploy/spl-token.ts
@@ -112,36 +112,3 @@ async function registerMetaData(tokenMintAccount: PublicKey) {
   const mintAddress = await mintToken();
   await registerMetaData(new PublicKey(mintAddress));
 })();
-
-// Anchor 버전
-// async function mintToken() {
-//   const mint = Keypair.generate();
-
-//   const space = MintLayout.span;
-//   const lamports = await connection.getMinimumBalanceForRentExemption(space);
-//   const createMintIx = SystemProgram.createAccount({
-//     fromPubkey: user.publicKey,
-//     newAccountPubkey: mint.publicKey,
-//     lamports,
-//     space,
-//     programId: SPL_PROGRAM_ID,
-//   });
-//   const transaction = new Transaction();
-//   const tx = transaction.add(createMintIx);
-//   await sendAndConfirmTransaction(connection, tx, [user, mint]);
-
-//   await tokenProgram.methods
-//     .initialize(6)
-//     .accounts({
-//       mint: mint.publicKey,
-//       authority: mint.publicKey,
-//       rent: SYSVAR_RENT_PUBKEY,
-//       system_program: SystemProgram.programId,
-//       token_program: SPL_PROGRAM_ID,
-//     })
-//     .signAndSend(user)
-//     .rpc();
-
-//   console.log("My Token CA:", mint.publicKey.toBase58());
-//   return mint.publicKey.toBase58();
-// }
